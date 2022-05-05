0b9937f25c9942c49e1f4d3e810023729c05a66a
Salma Gharssouan <salma.gharssouan@cdbdx.biz>
2022-05-05 16:08:34 +0200

    Test "ajout d'un montant negatif"

--- Banking.Tests.Unit/BankingTest.cs
+++ Banking.Tests.Unit/BankingTest.cs
@@ -3,50 +3,14 @@ using Xunit;
 
 namespace Banking.Tests.Unit
 {
-
     public class BankingTest
     {
+        private Account _account;
 
-        Account _account;
         public BankingTest()
         {
             _account = new Account();
         }
-
-        [Fact]
-        public void Should_increment_account_balance_when_deposing()
-        {
-            //Arrange
-            const int amount = 500;
-
-            //Act
-
-            _account.Deposit(amount, DateTime.Now);
-
-            //Assert
-
-            Assert.Equal(amount, _account.Balance);
-
-        }
-
-        [Fact]
-        public void Should_increment_account_balance_when_deposing_twice()
-        {
-            //Arrange
-            const int amountOne = 500;
-            const int amountTwo = 800;
-
-            //Act
-
-            _account.Deposit(amountOne, DateTime.Now);
-            _account.Deposit(amountTwo, DateTime.Now);
-
-            //Assert
-
-            Assert.Equal(amountOne + amountTwo, _account.Balance);
-
-        }
-
 
         [Fact]
         public void Should_decrement_account_balance_when_withdrawing()
@@ -61,7 +25,6 @@ namespace Banking.Tests.Unit
             //Assert
 
             Assert.Equal(-amount, _account.Balance);
-
         }
 
         [Fact]
@@ -79,8 +42,73 @@ namespace Banking.Tests.Unit
             //Assert
 
             Assert.Equal(-(amountOne + amountTwo), _account.Balance);
+        }
 
+        [Fact]
+        public void Should_increment_account_balance_when_deposing()
+        {
+            //Arrange
+            const int amount = 500;
+
+            //Act
+
+            _account.Deposit(amount, DateTime.Now);
+
+            //Assert
+
+            Assert.Equal(amount, _account.Balance);
+        }
+
+        [Fact]
+        public void Should_increment_account_balance_when_deposing_twice()
+        {
+            //Arrange
+            const int amountOne = 500;
+            const int amountTwo = 800;
+
+            //Act
+
+            _account.Deposit(amountOne, DateTime.Now);
+            _account.Deposit(amountTwo, DateTime.Now);
+
+            //Assert
+
+            Assert.Equal(amountOne + amountTwo, _account.Balance);
+        }
+
+        [Fact]
+        public void Should_sum_transaction_when_withdrawingAndDeposing()
+        {
+            //Arrange
+            const int amountToDeposit = 1000;
+            const int amountToWithdraw = 500;
+
+            //Act
+            _account.Deposit(amountToDeposit, DateTime.Now);
+            _account.Withdraw(amountToWithdraw, DateTime.Now);
+
+            //Assert
+            Assert.Equal(amountToDeposit - amountToWithdraw, _account.Balance);
+        }
+
+        [Fact]
+        public void Should_throw_exception_when_deposingNegativeAmount()
+        {
+            //Arrange
+            const int amountToDeposit = -500;
+            Exception exception = null;
+            //Act
+
+            try
+            {
+                _account.Deposit(amountToDeposit, DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            //Assert
+            Assert.NotNull(exception);
         }
     }
-
 }
